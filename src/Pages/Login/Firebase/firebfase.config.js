f3379b54049c4a87dda77984e919191504605619
alihaydarphpprogrammer <alihaydar6599@gmail.com>
2021-10-31 17:55:15 +0600

    done this workd

--- src/Pages/Login/Firebase/firebfase.config.js
+++ src/Pages/Login/Firebase/firebfase.config.js
@@ -1,11 +1,3 @@
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDLhWmMFO1KJe8kyU28XpMnbwsD0bmiCNg",
-//     authDomain: "haydar-medical-center.firebaseapp.com",
-//     projectId: "haydar-medical-center",
-//     storageBucket: "haydar-medical-center.appspot.com",
-//     messagingSenderId: "695325716379",
-//     appId: "1:695325716379:web:b007d1acbb56e7a9fd3b3e"
-// };
 
 const firebaseConfig = {
     apiKey: "AIzaSyCd9nIiOZ0ZOInN-b0VfqJVO2vBfQPyE94",
